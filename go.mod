5ddcde7ed2315cb7ab9e9773cc0a36097072d7eb
jonesjacklewis <jones.jack.lewis@gmail.com>
2025-07-14 12:21:52 +0100

    Added basic front end + cors

--- go.mod
+++ go.mod
@@ -5,3 +5,5 @@ go 1.23.0
 require github.com/mattn/go-sqlite3 v1.14.28
 
 require github.com/go-chi/chi/v5 v5.2.2
+
+require github.com/go-chi/cors v1.2.2
--- internal/api/routes.go
+++ internal/api/routes.go
@@ -4,10 +4,20 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/cors"
 )
 
 func (app *Application) Routes() http.Handler {
 	r := chi.NewRouter()
+
+	r.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"*"}, // For development, allow all origins
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any major browser
+	}))
 
 	r.Route("/podcasts", func(c chi.Router) {
 		c.Get("/", http.HandlerFunc(app.podcastsHandlerGet))
